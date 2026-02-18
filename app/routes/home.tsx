3a88a5160c414f7c690a1236a170083ba2582505
Kurganov1993 <kurganov.4n@yandex.ru>
2026-02-18 23:23:23 +0300

    hosting image

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,9 +1,11 @@
 import type { Route } from "./+types/home";
 import Navbar from "../../components/Navbar";
-import Button from "../../components/ui/Button";
 import {ArrowRight, ArrowUpRight, Clock, Layers} from "lucide-react";
-
-
+import Button from "../../components/ui/Button";
+import Upload from "../../components/Upload";
+import {useNavigate} from "react-router";
+import { useRef, useState} from "react";
+import {createProject} from "../../lib/puter.action";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -13,6 +15,49 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const navigate = useNavigate();
+  const [projects, setProjects] = useState<DesignItem[]>([]);
+  const isCreatingProjectRef = useRef(false);
+
+  const handleUploadComplete = async (base64Image: string) => {
+    try {
+
+      if(isCreatingProjectRef.current) return false;
+      isCreatingProjectRef.current = true;
+      const newId = Date.now().toString();
+      const name = `Residence ${newId}`;
+
+      const newItem = {
+        id: newId, name, sourceImage: base64Image,
+        renderedImage: undefined,
+        timestamp: Date.now()
+      }
+
+      const saved = await createProject({ item: newItem, visibility: 'private' });
+
+      if(!saved) {
+        console.error("Failed to create project");
+        return false;
+      }
+
+      setProjects((prev) => [saved, ...prev]);
+
+      navigate(`/visualizer/${newId}`, {
+        state: {
+          initialImage: saved.sourceImage,
+          initialRendered: saved.renderedImage || null,
+          name
+        }
+      });
+
+      return true;
+    } finally {
+      isCreatingProjectRef.current = false;
+    }
+  }
+
+
+
   return (
       <div className="home">
         <Navbar />
@@ -55,7 +100,7 @@ export default function Home() {
                 <p>Supports JPG, PNG, formats up to 10MB</p>
               </div>
 
-              <p>Upload images</p>
+              <Upload onComplete={handleUploadComplete} />
             </div>
           </div>
         </section>
@@ -70,11 +115,10 @@ export default function Home() {
             </div>
 
             <div className="projects-grid">
-                  <div  className="project-card group">
+              {projects.map(({id, name, renderedImage, sourceImage, timestamp}) => (
+                  <div key={id} className="project-card group" onClick={() => navigate(`/visualizer/${id}`)}>
                     <div className="preview">
-                      <img
-                          src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png"
-                            alt="Project"
+                      <img  src={renderedImage || sourceImage} alt="Project"
                       />
 
                       <div className="badge">
@@ -84,12 +128,12 @@ export default function Home() {
 
                     <div className="card-body">
                       <div>
-                        <h3>Project Mannnn</h3>
+                        <h3>{name}</h3>
 
                         <div className="meta">
                           <Clock size={12} />
-                          <span>{new Date('01.01.2026').toLocaleDateString()}</span>
-                          <span>By Andrew</span>
+                          <span>{new Date(timestamp).toLocaleDateString()}</span>
+                          <span>By JS Mastery</span>
                         </div>
                       </div>
                       <div className="arrow">
@@ -97,6 +141,7 @@ export default function Home() {
                       </div>
                     </div>
                   </div>
+              ))}
             </div>
           </div>
         </section>
